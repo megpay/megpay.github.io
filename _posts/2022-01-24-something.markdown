48a52e0f485616b7abd755029c5643dc2f8a0edb
Megan Payne <mpayne@bebr.nl>
2022-02-05 17:24:37 +0100

    Site change

---
+++ _posts/2022-01-24-something.markdown
@@ -0,0 +1,6 @@
+---
+layout: article
+title:  Romio and Juliet are dead... still
+date:   2022-01-24 00:13:37 +0100
+categories: Python algorithms
+---
--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"The Advent of Code in January?",'url':"/python/algorithms/2022/01/23/advent-of-code.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"The Advent of Code in January?",'url':"/python/algorithms/2022/01/23/advent-of-code.html"},{'title':"Romio and Juliet are dead... still",'url':"/python/algorithms/2022/01/24/something.html"}]};
